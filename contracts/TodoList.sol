8a669562293f6c7e2a2a3b778e054093c9130469
lancelee2885 <lancelee2885@gmail.com>
2021-11-18 23:17:36 -0800

    created struct for task and function to add tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,7 +2,21 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0; // state variable, written into blockchain
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out Lance's Profile");
+    }
+
+    function createTask( string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
-
-
-
